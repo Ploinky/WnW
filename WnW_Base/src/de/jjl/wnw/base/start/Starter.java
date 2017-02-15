0db10c1bae169c654a8b9d565aca36c43098522e
johannes.litger <johannes.litger@Litger.utintra.com>
2017-02-15 12:11:17 +0100

    gitignore

---
+++ WnW_Base/src/de/jjl/wnw/base/start/Starter.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2017 Unitechnik Systems GmbH. All Rights Reserved.
+ */
+package de.jjl.wnw.base.start;
+
+public class Starter
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
