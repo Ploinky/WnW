a1e242d32c7ddd3182e950fa3f19a91f0b296957
johannes.litger <johannes.litger@Freds_PC.Speedport_W_724V_Typ_A_05011603_05_020>
2017-02-15 17:00:07 +0100

    gitignore

--- WnW_Desktop/src/de/jjl/wnw/desktop/start/Starter.java
+++ WnW_Desktop/src/de/jjl/wnw/desktop/start/Starter.java
@@ -11,8 +11,9 @@ public class Starter extends Application
 	public static void main(String[] args)
 	{
 		launch(args);
+
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
