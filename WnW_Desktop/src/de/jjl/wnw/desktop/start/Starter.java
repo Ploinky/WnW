9bc5dffe9b3fc2dd962a12928e119d680905c445
johannes.litger <johannes.litger@Litger.utintra.com>
2017-02-15 12:29:58 +0100

    ???

---
+++ WnW_Desktop/src/de/jjl/wnw/desktop/start/Starter.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2017 Unitechnik Systems GmbH. All Rights Reserved.
+ */
+package de.jjl.wnw.desktop.start;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application
+{
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
