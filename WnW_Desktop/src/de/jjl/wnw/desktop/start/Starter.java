cbaf8d6801c6517338ade7bcab482c3bd37b1d8a
johannes.litger <johannes.litger@Litger.utintra.com>
2017-02-15 12:32:53 +0100

    fixed gitignore

--- WnW_Desktop/src/de/jjl/wnw/desktop/start/Starter.java
+++ WnW_Desktop/src/de/jjl/wnw/desktop/start/Starter.java
@@ -8,11 +8,15 @@ import javafx.stage.Stage;
 
 public class Starter extends Application
 {
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		// TODO Auto-generated method stub
-
+		primaryStage.show();
 	}
 
 }
