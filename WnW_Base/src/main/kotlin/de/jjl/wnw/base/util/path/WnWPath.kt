83e256dfc7a22949e5849de5fda5c7d59a764ee1
Dauerdaddlah <hertel-dennis@web.de>
2017-09-25 14:49:44 +0200

    moved some method-bodies to interface created WnWPath-subclass Trimmed to reduce overhead if trim() is used frequently

--- WnW_Base/src/main/kotlin/de/jjl/wnw/base/util/path/WnWPath.kt
+++ WnW_Base/src/main/kotlin/de/jjl/wnw/base/util/path/WnWPath.kt
@@ -10,35 +10,89 @@ interface WnWPath : Iterable<WnWPoint>
 	val system: WnWDisplaySystem
 	
 	val pathMinX: Int
+		get() = map { it.x } .min() ?: 0
 	val pathMaxX: Int
+		get() = map { it.x } .max() ?: 0
 	val pathMinY: Int
+		get() = map { it.y } .min() ?: 0
 	val pathMaxY: Int
+		get() = map { it.y } .max() ?: 0
 	val pathWidth: Int
+		get() = pathMaxX - pathMinX
 	val pathHeight: Int
+		get() = pathMaxY - pathMinY
 	
 	fun forSystem(system: WnWDisplaySystem): WnWPath
-	fun trimmed(): WnWPath
-	fun trimmedToSize(width: Int, height: Int): WnWPath
+	
+	fun trimmed(): WnWPath = Trimmed(this)
+	fun trimmedToSize(width: Int, height: Int): WnWPath = trimmed().forSystem(WnWDisplaySystem(width, height, system.xAxis, system.yAxis))
 }
 
 open class WnWPathSimple(override val system: WnWDisplaySystem) : WnWPath
 {
 	val points = arrayListOf<WnWPoint>()
 	
-	override val pathMinX
-		get() = if(points.isEmpty()) 0 else points.minBy { it.x }!!.x 
-	override val pathMinY
-		get() = if(points.isEmpty()) 0 else points.minBy { it.y }!!.y
-	override val pathMaxX
-		get() = if(points.isEmpty()) 0 else points.maxBy { it.x }!!.x
-	override val pathMaxY
-		get() = if(points.isEmpty()) 0 else points.maxBy { it.y }!!.y
-	override val pathWidth
-		get() = pathMaxX - pathMinX
-	override val pathHeight
-		get() = pathMaxY - pathMinY
-	
 	override fun iterator(): Iterator<WnWPoint> = points.iterator()
+	
+	override open fun forSystem(system: WnWDisplaySystem): WnWPath
+	{
+		if(system == this.system)
+		{
+			return this;
+		}
+		
+		val path = WnWPathSimple(system)
+		var lastPoint: WnWPoint? = null
+		
+		for(point in this)
+		{
+			val distX = point.x - this.system.zeroX
+			val distY = point.y - this.system.zeroY
+			val distXRel = distX / this.system.width
+			val distYRel = distY / this.system.height
+			
+			val nextPoint = WnWPoint(
+								system.zeroX + (system.width * distXRel),
+								system.zeroY + (system.height * distYRel))
+			
+			if(nextPoint != lastPoint)
+			{
+				path.addPoint(nextPoint)
+				
+				lastPoint = nextPoint
+			}
+		}
+		
+		return path;
+	}
+	
+	fun addPoint(x: Int, y: Int): Unit = addPoint(WnWPoint(x, y))
+	open fun addPoint(point: WnWPoint): Unit { points.add(point) }
+}
+
+class Trimmed(path: WnWPath) : WnWPath
+{
+	override val system = WnWDisplaySystem(path.pathWidth, path.pathHeight, path.system.xAxis, path.system.yAxis)
+	val points: List<WnWPoint>
+	
+	override val pathMinX = 0
+	override val pathMaxX = system.width
+	override val pathMinY = 0
+	override val pathMaxY = system.height
+	
+	init
+	{
+		val min = WnWPoint(path.pathMinX, path.pathMinY)
+		var lastPoint: WnWPoint? = null
+		
+		points = path.map { it - min }
+					.filter {
+						val l = lastPoint
+						lastPoint = it
+						it != l
+					}
+					.toList()
+	}
 	
 	override fun forSystem(system: WnWDisplaySystem): WnWPath
 	{
@@ -71,28 +125,7 @@ open class WnWPathSimple(override val system: WnWDisplaySystem) : WnWPath
 		return path;
 	}
 	
-	fun addPoint(x: Int, y: Int): Unit = addPoint(WnWPoint(x, y))
-	open fun addPoint(point: WnWPoint): Unit { points.add(point) }
+	override fun trimmed() = this
 	
-	override fun trimmedToSize(width: Int, height: Int) = trimmed().forSystem(WnWDisplaySystem(width, height, system.xAxis, system.yAxis))
-	
-	override fun trimmed(): WnWPath
-	{
-		val min = WnWPoint(pathMinX, pathMinY)
-		val path = WnWPathSimple(WnWDisplaySystem(pathWidth, pathHeight, system.xAxis, system.yAxis))
-		
-		var lastPoint: WnWPoint? = null
-		for(point in this)
-		{
-			val nextPoint = point - min
-			
-			if(nextPoint != lastPoint)
-			{
-				path.addPoint(nextPoint)
-				lastPoint = nextPoint
-			}
-		}
-		
-		return path
-	}
+	override fun iterator(): Iterator<WnWPoint> = points.iterator()
 }
