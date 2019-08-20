d475b708fae1f1d838e89c6da24f00fd78d571fe
mail <mail@DESKTOP-U99K6F8.Speedport_W_724V_Typ_A_05011603_06_001>
2019-08-20 21:40:15 +0200

    Adding color to spells

--- WnW_Base/src/main/java/de/jjl/wnw/dev/spell/SpellUtil.java
+++ WnW_Base/src/main/java/de/jjl/wnw/dev/spell/SpellUtil.java
@@ -30,8 +30,10 @@ public class SpellUtil
 
 	private void load()
 	{
-		SpellFactory spell = new SpellFactory("test_spell", 1, new long[] { 753, 159, 14789 });
-		spells.add(spell);
+		SpellFactory spell = new SpellFactory("test_spell", 1, new long[] {753});
+		SpellFactory spell1 = new SpellFactory("test_spell1", 2, new long[] {159});
+		SpellFactory spell2 = new SpellFactory("test_spell2", 3, new long[] {14789});
+		spells.addAll(Arrays.asList(spell, spell1, spell2));
 	}
 
 	public static Spell getSpell(Player player1, List<Long> combo, boolean shield)
--- WnW_Desktop/src/main/java/de/jjl/wnw/desktop/gui/frames/DesktopObjectPainter.java
+++ WnW_Desktop/src/main/java/de/jjl/wnw/desktop/gui/frames/DesktopObjectPainter.java
@@ -1,5 +1,7 @@
 package de.jjl.wnw.desktop.gui.frames;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -19,18 +21,19 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
-public class DesktopObjectPainter 
+public class DesktopObjectPainter
 {
 	private GraphicsContext graphics;
-	
+
 	private Image playerSprite;
-	
+
 	private Map<Long, Image> runeSprites;
-	
+	private Map<String, Image> spellSprites;
+
 	public DesktopObjectPainter(GraphicsContext graphics)
 	{
 		this.graphics = graphics;
-		
+
 		try
 		{
 			loadResources();
@@ -44,48 +47,64 @@ public class DesktopObjectPainter
 
 	private void loadResources() throws IOException
 	{
-			playerSprite = SwingFXUtils.toFXImage(ImageIO.read(getClass()
-					.getResourceAsStream("/img/Lil' Wizzy Cropped.png")), null);
-			
-			runeSprites = new HashMap<>();
-			// TODO $Li 23.02.2019 boy we REALLY need to fix this!
-			runeSprites.put(14789l, SwingFXUtils.toFXImage(ImageIO.read(new File("res/14789.png")), new WritableImage(40, 40)));
-			runeSprites.put(159l, SwingFXUtils.toFXImage(ImageIO.read(new File("res/159.png")), new WritableImage(40, 40)));
-			runeSprites.put(753l, SwingFXUtils.toFXImage(ImageIO.read(new File("res/753.png")), new WritableImage(40, 40)));
+		playerSprite = SwingFXUtils
+				.toFXImage(ImageIO.read(getClass().getResourceAsStream("/img/Lil' Wizzy Cropped.png")), null);
 
+		runeSprites = new HashMap<>();
+		// TODO $Li 23.02.2019 boy we REALLY need to fix this!
+		runeSprites.put(14789l,
+				SwingFXUtils.toFXImage(ImageIO.read(new File("res/14789.png")), new WritableImage(40, 40)));
+		runeSprites.put(159l, SwingFXUtils.toFXImage(ImageIO.read(new File("res/159.png")), new WritableImage(40, 40)));
+		runeSprites.put(753l, SwingFXUtils.toFXImage(ImageIO.read(new File("res/753.png")), new WritableImage(40, 40)));
+
+		spellSprites = new HashMap<>();
+		BufferedImage image = new BufferedImage(30, 30, BufferedImage.TYPE_INT_BGR);
+		Graphics2D graphics = image.createGraphics();
+		graphics.setColor(java.awt.Color.red);
+		graphics.fillRect(0, 0, 30, 30);
+
+		spellSprites.put("159", SwingFXUtils.toFXImage(image, null));
+
+		graphics.setColor(java.awt.Color.green);
+		graphics.fillRect(0, 0, 30, 30);
+		spellSprites.put("753", SwingFXUtils.toFXImage(image, null));
+
+		graphics.setColor(java.awt.Color.blue);
+		graphics.fillRect(0, 0, 30, 30);
+		spellSprites.put("14789", SwingFXUtils.toFXImage(image, null));
 	}
-	
+
 	private void drawPlayer(GamePlayer player)
 	{
-		graphics.drawImage(playerSprite, player.getX() + (player.isFaceLeft() ? player.getWidth() / 2 : player.getWidth() / -2),
-				player.getY() - player.getHeight() / 2,
-				player.isFaceLeft() ? -player.getWidth() : player.getWidth(),
+		graphics.drawImage(playerSprite,
+				player.getX() + (player.isFaceLeft() ? player.getWidth() / 2 : player.getWidth() / -2),
+				player.getY() - player.getHeight() / 2, player.isFaceLeft() ? -player.getWidth() : player.getWidth(),
 				player.getHeight());
 		graphics.setFont(new Font(20));
 		graphics.setFill(Color.BLACK);
-		graphics.fillText("" + player.getLives(), player.getX(), player.getY());	
+		graphics.fillText("" + player.getLives(), player.getX(), player.getY());
 	}
-	
+
 	public void draw(GameObject obj)
 	{
-		if(obj instanceof Spell)
+		if (obj instanceof Spell)
 		{
 			drawSpell((Spell) obj);
 		}
-		else if(obj instanceof GamePlayer)
+		else if (obj instanceof GamePlayer)
 		{
 			drawPlayer((GamePlayer) obj);
 		}
-		else if(obj instanceof BaseRune)
+		else if (obj instanceof BaseRune)
 		{
 			drawRune((BaseRune) obj);
 		}
-		else if(obj instanceof Drawable)
+		else if (obj instanceof Drawable)
 		{
 			((Drawable) obj).drawOn(graphics);
 		}
 	}
-	
+
 	private void drawRune(BaseRune rune)
 	{
 		graphics.drawImage(runeSprites.get(rune.getLong()), rune.getX(), rune.getY(), 30, 30);
@@ -93,7 +112,13 @@ public class DesktopObjectPainter
 
 	private void drawSpell(Spell spell)
 	{
-		graphics.fillRect(spell.getX() - spell.getWidth() / 2, spell.getY() - spell.getHeight() / 2,
-				spell.getWidth(), spell.getHeight());
+		String s = "";
+
+		for (long l : spell.getSpellCombo())
+		{
+			s += l;
+		}
+
+		graphics.drawImage(spellSprites.get(s), spell.getX(), spell.getY(), 30, 30);
 	}
 }
